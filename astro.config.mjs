eb87c0ee8555f199ecafbfdc1aef0bd339e2821e
fmfm2024 <fairymade2024@gmail.com>
2024-12-20 17:26:31 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,17 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
     server:{
         host:true,
-        // headers: {
-        //     "Content-Security-Policy": [
-        //         "default-src 'self';",
-        //         "script-src 'self' https://challenges.cloudflare.com https://sandbox-merchant.revolut.com 'unsafe-inline';",
-        //         "style-src 'self' https://fonts.googleapis.com 'unsafe-inline';",
-        //         `img-src 'self' data: https://link.storjshare.io ${process.env.PUBLIC_FM_PUBLIC_IMAGE_URL_PREFIX} ${process.env.PUBLIC_FM_DELIVERY_PROOF_IMAGE_URL_PREFIX};`,
-        //         "frame-src 'self' https://challenges.cloudflare.com https://sandbox-merchant.revolut.com;",
-        //         "font-src 'self' https://fonts.gstatic.com;",
-        //         `connect-src 'self' ${process.env.FM_WEBSOCKET_URL}`,
-        //     ].join(' ')
-        // },
+        headers: {
+            "Content-Security-Policy": [
+                "default-src 'self';",
+                "script-src 'self' https://challenges.cloudflare.com https://sandbox-merchant.revolut.com 'unsafe-inline';",
+                "style-src 'self' https://fonts.googleapis.com 'unsafe-inline';",
+                `img-src 'self' data: https://link.storjshare.io ${process.env.PUBLIC_FM_PUBLIC_IMAGE_URL_PREFIX} ${process.env.PUBLIC_FM_DELIVERY_PROOF_IMAGE_URL_PREFIX};`,
+                "frame-src 'self' https://challenges.cloudflare.com https://sandbox-merchant.revolut.com;",
+                "font-src 'self' https://fonts.gstatic.com;",
+                `connect-src 'self' ${process.env.FM_WEBSOCKET_URL}`,
+            ].join(' ')
+        },
     },
     integrations: [
         solid(),
