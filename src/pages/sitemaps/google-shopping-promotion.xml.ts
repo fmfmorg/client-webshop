56751a087b73bdb833636c65b73f225b46490d7d
fmfm2024 <fairymade2024@gmail.com>
2024-12-26 19:34:00 +0000

    .

--- src/pages/sitemaps/google-shopping-promotion.xml.ts
+++ src/pages/sitemaps/google-shopping-promotion.xml.ts
@@ -11,12 +11,6 @@ export async function GET({url}:{url:URL}) {
     })
     if (!resp.ok) return new Response(null, {status:500})
     const { campaigns } = await resp.json() as { campaigns: string; }
-    // const sitemap = `
-    //     <?xml version="1.0"?>
-    //     <promotions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="promotions.xsd">
-    //         ${campaigns}
-    //     </promotions>
-    // `.trim();
 
     const sitemap = `
             <?xml version="1.0"?>
