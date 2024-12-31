f3bfeea07e6a1e26741490b8e705fb886d9347e8
fmfm2024 <fairymade2024@gmail.com>
2024-12-31 17:22:08 +0000

    .

--- src/components/catalogue-item/interfaces.tsx
+++ src/components/catalogue-item/interfaces.tsx
@@ -14,6 +14,7 @@ export interface IProduct {
     id:string;
     name:string;
     description:string;
+    metaDescription:string;
     specification?:string;
     price:number;
     discountedPrice:number;
