2d8341a91edbe53540b70a92fc9f9920bffaa727
fmfm2024 <fairymade2024@gmail.com>
2025-01-01 20:01:12 +0000

    .

--- src/components/cart/image.tsx
+++ src/components/cart/image.tsx
@@ -15,7 +15,6 @@ const Image = (
         {
             [
                 '.avif',
-                '.webp',
                 ext
             ].map(e=>(
                 e === ext ?
--- src/components/catalogue-item/image-style.ts
+++ src/components/catalogue-item/image-style.ts
@@ -1,3 +1,3 @@
 import { PUBLIC_FM_PUBLIC_IMAGE_URL_PREFIX } from 'astro:env/client'
 
-export const getImageStyle = (name:string,ext:string,imageSize?:number) => `image-set(${['.avif','.webp',ext].map(e=>`url(${PUBLIC_FM_PUBLIC_IMAGE_URL_PREFIX}${name}${!!imageSize ? `_${imageSize}x${imageSize}` : ''}${e}) type('image/${e === '.jpg' ? 'jpeg' : e.replace('.','').toLowerCase()}')`).join(', ')})`
+export const getImageStyle = (name:string,ext:string,imageSize?:number) => `image-set(${['.avif',ext].map(e=>`url(${PUBLIC_FM_PUBLIC_IMAGE_URL_PREFIX}${name}${!!imageSize ? `_${imageSize}x${imageSize}` : ''}${e}) type('image/${e === '.jpg' ? 'jpeg' : e.replace('.','').toLowerCase()}')`).join(', ')})`
