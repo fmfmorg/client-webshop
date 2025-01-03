c085df2419594d1d468e05aae3b64eb76494b199
fmfm2024 <fairymade2024@gmail.com>
2025-01-03 17:59:21 +0000

    .

--- src/misc/session-lost.ts
+++ src/misc/session-lost.ts
@@ -5,6 +5,7 @@ const bc = new BroadcastChannel('reload')
 export const sessionLost = async (status:number) => {
     if (status === 403) {
         bc.postMessage(pageToken.get())
+        bc.close()
         if (signedIn.get()){
             const { pathname, search } = window.location
             window.location.assign(`/sign-in?rd=${encodeURIComponent(pathname + search)}`)
