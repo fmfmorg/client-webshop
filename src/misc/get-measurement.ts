43cf540f7df17be4f675298f11588c239d1c7625
fmfm2024 <fairymade2024@gmail.com>
2025-01-21 22:49:07 +0000

    .

--- src/misc/get-measurement.ts
+++ src/misc/get-measurement.ts
@@ -2,12 +2,14 @@ import type { IProductMeasurement } from "./interfaces"
 
 export const getMeasurement = (m:IProductMeasurement[],key:'width'|'depth'|'height'|'weight',soldAsPair:boolean) => {
   if (!m || !m.length) return 0
+  let result = 0
   if (key === 'weight') {
     if (soldAsPair) {
-      if (m.length === 1) return m[0].weight * 2
-      else return m.map(e=>e[key]).reduce((a,b)=>a+b,0)
+      if (m.length === 1) result = m[0].weight * 2
+      else result = m.map(e=>e[key]).reduce((a,b)=>a+b,0)
     } else {
-      return m.find(e=>!!e[key])[key]
+      result = m.find(e=>!!e[key])[key]
     }
-  } else return Math.max(...m.map(e=>e[key]))
+  } else result = Math.max(...m.map(e=>e[key]))
+  return Math.round(result)
 }
